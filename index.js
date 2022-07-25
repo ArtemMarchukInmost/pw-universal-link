3420cbc413e54865cdf0c83712d64e3e5320b715
Artem <artem.marchuk@inmost.pro>
2022-07-25 21:45:53 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.get('/apple-app-site-association', (req, res) => {
+app.get('/.well-known/apple-app-site-association', (req, res) => {
     const options = {
         root: path.join(__dirname)
     };
 
     res.type('application/pkcs7-mime');
+    res.status(200);
     res.sendFile('apple-app-site-association.json', options)
 });
 
