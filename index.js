75f48442bc75d448f94ae68fa77d224d041d8b9b
Artem <artem.marchuk@inmost.pro>
2022-07-27 13:51:10 +0300

    feat

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.get('/.well-known/apple-app-site-association', (req, res) => {
     res.sendFile('apple-app-site-association.json', options)
 });
 
+app.get('/.well-known/assetlinks.json', (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
+
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile('assetlinks.json', options)
+});
+
 app.listen(PORT, () => {
     console.log('server has been started');
 });
