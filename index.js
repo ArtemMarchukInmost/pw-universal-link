3c9fff5c257c6e4107f57bfd2c77823078bad9ae
Artem <artem.marchuk@inmost.pro>
2022-08-02 23:33:38 +0300

    c

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+
+app.use(bodyParser());
 
 app.get('', (req, res) => {
     res.status(200);
